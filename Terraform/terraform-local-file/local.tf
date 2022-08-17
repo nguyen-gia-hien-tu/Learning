713285173f591c97df104d685e563adc30ff7b30
Hien Tu <giahien196@gmail.com>
2022-08-16 20:19:11 -0400

    Update files for Terraform

--- Terraform/terraform-local-file/local.tf
+++ Terraform/terraform-local-file/local.tf
@@ -3,3 +3,14 @@ resource "local_file" "pet" {
     content = "We love pets!!!"
     file_permission = "0700"
 }
+
+resource "local_file" "cat" {
+    filename = "cat.txt"
+    content = "CATTTTTTTT, MEOWWWWW!!!!"
+}
+
+resource "random_pet" "my-pet" {
+    prefix = "Mrs"
+    separator = "."
+    length = "1"
+}
