2ba2976d41d9ca072d9243f35a2306c1a34015f3
Hien Tu <giahien196@gmail.com>
2022-08-18 21:56:08 -0400

    Add file to learn Terraform

--- Terraform/terraform-basics/main.tf
+++ Terraform/terraform-basics/main.tf
@@ -1,9 +1,19 @@
 resource "local_file" "greet" {
-    filename = "greet.txt"
-    content = "Hello ${var.name}"
+  filename = "greet.txt"
+  content  = "Hello ${var.name}"
 }
 
 resource "local_file" "greet-pet" {
-    filename = "greet-pet.txt"
-    content = var.pet-name
+  filename = "greet-pet.txt"
+  content  = var.pet-name
+}
+
+resource "local_file" "greet-me" {
+    filename = "greet-me.txt"
+    # Since "my-name" variable is in the "vars.tfvars" file, which is not
+    # auto-loaded by Terraform, we need to load it in the terminal when
+    # executing "terraform plan" or "terraform apply" by adding "-var-file
+    # <file_name>" (i.e., "terraform plan -var-file vars.tfvars" and "terraform
+    # apply -var-file vars.tfvars")
+    content = var.my-name
 }
--- Terraform/terraform-basics/variables.tf
+++ Terraform/terraform-basics/variables.tf
@@ -1,8 +1,12 @@
 variable "name" {
-    type = string
-    default = "Hien"
+  type    = string
+  default = "Hien"
 }
 
 variable "pet-name" {
+  type = string
+}
+
+variable "my-name" {
     type = string
 }
